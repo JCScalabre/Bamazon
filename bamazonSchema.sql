fb2a7106d817230f6fec47630a59ed8395f00405
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-22 23:18:39 -0500

    readme included

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -5,6 +5,7 @@ USE bamazon;
 CREATE TABLE products(
 	item_id INT AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
+    product_name_plural VARCHAR (30) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
     price INT,
     stock_quantity INT,
--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -1,12 +1,12 @@
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Apple", "Fruit", 2, 18),
-	   ("Banana", "Fruit", 1.5, 22),
-       ("Strawberry", "Fruit", 3, 30),
-       ("Orange", "Fruit", 2.5, 15),
-       ("Carrot", "Vegetable", 2, 30),
-       ("Lettuce", "Vegetable", 1, 25),
-       ("Onion", "Vegetable", 2, 20),
-       ("Chicken", "Meat", 3.5, 10),
-       ("Steak", "Meat", 4, 10),
-       ("Pork", "Meat", 3.5, 10);
+INSERT INTO products (product_name, product_name_plural, department_name, price, stock_quantity)
+VALUES ("Apple", "apples", "Fruit", 2, 18),
+	   ("Banana", "bananas", "Fruit", 1.5, 5),
+       ("Strawberry", "strawberries", "Fruit", 3, 30),
+       ("Orange", "oranges", "Fruit", 2.5, 15),
+       ("Carrot", "carrots", "Vegetable", 2, 30),
+       ("Lettuce", "units of lettuce", "Vegetable", 1, 25),
+       ("Onion", "onions", "Vegetable", 2, 20),
+       ("Chicken", "pieces of chicken", "Meat", 3.5, 10),
+       ("Steak", "steaks", "Meat", 4, 10),
+       ("Pork", "pieces of pork", "Meat", 3.5, 10);
        
